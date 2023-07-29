082f191df71947ef224a323d8fb8d046d301af8b
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-07-29 16:44:14 -0300

    🚧 create a method to set the documentation ui

--- src/main/config/documentation.config.ts
+++ src/main/config/documentation.config.ts
@@ -1,4 +1,5 @@
 import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 import { type FastifyInstance } from 'fastify'
 
 export const SetDocumentation = async (app: FastifyInstance): Promise<void> => {
@@ -11,12 +12,26 @@ export const SetDocumentation = async (app: FastifyInstance): Promise<void> => {
       },
       externalDocs: {
         url: 'https://swagger.io',
-        description: 'You will find more info here'
+        description: 'Find more info here'
       },
       host: 'localhost',
       schemes: ['http'],
       consumes: ['application/json'],
       produces: ['application/json']
     }
+  })
+}
+
+export const SetDocumentationUI = async (app: FastifyInstance): Promise<void> => {
+  await app.register(swaggerUi, {
+    routePrefix: '/documentation',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
   })
 }
