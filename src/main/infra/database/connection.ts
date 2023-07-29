4dc563a3c9c8d2839821b9e7b29f25868349bed9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-07-29 17:47:17 -0300

    🚧 add auth properties to database connection

--- src/main/infra/database/connection.ts
+++ src/main/infra/database/connection.ts
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose'
 export const Connect = async (): Promise<mongoose.Mongoose> => {
-  const { HOST, PORT, DATABASE } = process.env
+  const { HOST, PORT, DATABASE, DB_USER, DB_PASS } = process.env
 
-  return await mongoose.connect(`${HOST}:${PORT}/${DATABASE}`)
+  return await mongoose
+    .connect(
+      `mongodb://${HOST}:${PORT}/`,
+      {
+        user: DB_USER,
+        pass: DB_PASS,
+        dbName: DATABASE
+      }
+    )
 }
