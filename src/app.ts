669b5eb132a5ff003033f399f8c707dffc197e48
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-07-29 16:30:14 -0300

    🚧 add the new method to start the application

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
 import 'reflect-metadata'
-import fastify, { type FastifyInstance, type FastifyReply, type FastifyRequest } from 'fastify'
+import { Start } from './main/config/app.config'
 
-const app: FastifyInstance = fastify({
-  logger: { level: 'info' }
-})
-
-app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-  await reply.send({ statusCode: 200, body: { message: 'Hello, World!' } })
-})
-
-app
-  .listen({ port: 3001 })
-  .then((res) => {
-    app.log.info(`Server is running on ${res}.`)
-  })
-  .catch((err) => {
-    app.log.error((err as Error).message)
-  })
+Start().then()
