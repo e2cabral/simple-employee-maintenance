30a6d23fe193e2cc2e85d546c79580ac762c5548
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-07-31 17:51:51 -0300

    🚧 create specific interfaces

--- src/main/documentation/routes/documentation.types.ts
+++ src/main/documentation/routes/documentation.types.ts
@@ -4,3 +4,19 @@ export interface DefaultBody<T> {
   properties?: T
   items?: DefaultBody<T>
 }
+
+export interface ErrorBody extends DefaultBody<any> {
+  type: string
+  properties: {
+    message: { type: string }
+    statusCode: { type: string, default: number }
+  }
+}
+
+export interface NoContentBody extends DefaultBody<any> {
+  type: string
+  properties: {
+    message: { type: string }
+    statusCode: { type: string, default: number }
+  }
+}
