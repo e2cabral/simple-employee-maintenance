94ece32d11941613911759a74ae2fbccc2c30fe9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-07-31 16:55:36 -0300

    🚧 add a method to get the default response to get by id method

--- src/main/documentation/routes/employees/default-response.route.ts
+++ src/main/documentation/routes/employees/default-response.route.ts
@@ -18,3 +18,17 @@ export const defaultGetResponse = (): DefaultBody<EmployeeGetResponse> => {
     }
   }
 }
+export const defaultGetByIdResponse = (): DefaultBody<EmployeeGetResponse> => {
+  return {
+    description: 'Retrieve employees paginated',
+    type: 'object',
+    properties: {
+      firstName: { type: 'string' },
+      fastName: { type: 'string' },
+      hireDate: { type: 'string' },
+      department: { type: 'string' },
+      phone: { type: 'string' },
+      address: { type: 'string' }
+    }
+  }
+}
