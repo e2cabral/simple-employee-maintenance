c712133d495e35d5c4133d0a0903de912fd018bb
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-07-31 16:35:25 -0300

    💡 remove todo comment

--- src/infra/database/connection.ts
+++ src/infra/database/connection.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose'
 
-// TODO: Just to commit to a new repository
 export const Connect = async (): Promise<mongoose.Mongoose> => {
   const { HOST, PORT, DATABASE, DB_USER, DB_PASS } = process.env
 
