6e041f7124c0617968f5132adebfa13e4669d3de
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-07-31 16:30:37 -0300

    🚧 add a method to get the default response serialization

---
+++ src/main/documentation/routes/employees/default-response.route.ts
@@ -0,0 +1,20 @@
+import { type DefaultBody } from '../documentation.types'
+import { type EmployeeGetResponse } from './index'
+
+export const defaultGetResponse = (): DefaultBody<EmployeeGetResponse> => {
+  return {
+    description: 'Retrieve employees paginated',
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        firstName: { type: 'string' },
+        fastName: { type: 'string' },
+        hireDate: { type: 'string' },
+        department: { type: 'string' },
+        phone: { type: 'string' },
+        address: { type: 'string' }
+      }
+    }
+  }
+}
